beba1eba3ddb71db4e4370cd65449a6e8eea7f11
nurynurish <potterharry200200.com>
2022-07-31 20:45:19 +0200

    Completed sub total veg. matters

--- script.js
+++ script.js
@@ -7,4 +7,34 @@ tomatoPrice = document.getElementById("tomatoPrice").value;
 tomatoAmount= document.getElementById("tomatoAmount").value;
 tomatoResult = tomatoPrice * tomatoAmount;
 document.getElementById("tomatoResult").innerHTML = tomatoResult;
+
+///////////////////////////////////PEPPER//////////////////////////////
+
+event.preventDefault();
+var pepperPrice;
+var pepperAmount;
+var pepperResult;
+pepperPrice = document.getElementById("pepperPrice").value;
+pepperAmount= document.getElementById("pepperAmount").value;
+pepperResult = tomatoPrice * tomatoAmount;
+document.getElementById("pepperResult").innerHTML = tomatoResult;
+
+///////////////////////////////////EGGPLANT//////////////////////////////
+
+
+event.preventDefault();
+var eggplantPrice;
+var eggplantAmount;
+var eggplantResult;
+eggplantPrice = document.getElementById("eggplantPrice").value;
+eggplantAmount= document.getElementById("eggplantAmount").value;
+eggplantResult = eggplantPrice * eggplantAmount;
+document.getElementById("eggplantResult").innerHTML = eggplantResult;
+
+///////////////////////////////////////////////////////////////////////
+
+var subtotal_veg;
+subtotal_veg = tomatoResult + pepperResult + eggplantResult;
+document.getElementById("subtotal_veg").innerHTML = subtotal_veg;
+
 }
