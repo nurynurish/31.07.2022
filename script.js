9efef11c9b45864b2dbdb3b9aeb88ab7a8fd2566
nurynurish <potterharry200200.com>
2022-08-02 20:02:31 +0200

    Finished stage of the product (Product Calculator)

--- script.js
+++ script.js
@@ -47,4 +47,38 @@ cheesePrice = document.getElementById("cheesePrice").value;
 cheeseAmount= document.getElementById("cheeseAmount").value;
 cheeseResult = cheesePrice * cheeseAmount;
 document.getElementById("cheeseResult").innerHTML = cheeseResult;
+
+
+///////////////BUTTER////////////////////////////////////////////
+
+event.preventDefault();
+var butterPrice;
+var butterAmount;
+var butterResult;
+butterPrice = document.getElementById("butterPrice").value;
+butterAmount= document.getElementById("butterAmount").value;
+butterResult = butterPrice * butterAmount;
+document.getElementById("butterResult").innerHTML = butterResult;
+///////////////MILK - BOX////////////////////////////////////////////
+
+event.preventDefault();
+var milk_boxPrice;
+var milk_boxAmount;
+var milk_boxResult;
+milk_boxPrice = document.getElementById("milk_boxPrice").value;
+milk_boxAmount= document.getElementById("milk_boxAmount").value;
+milk_boxResult = milk_boxPrice * milk_boxAmount;
+document.getElementById("milk_boxResult").innerHTML = milk_boxResult;
+
+///////////////SUB-TOTAL////////////////////////////////////////////////////////
+
+var subtotal;
+subtotal = cheeseResult + butterResult + milk_boxResult;
+document.getElementById("subtotal").innerHTML = subtotal;
+
+//////////////////PLUS TWO SIDES//////////////////////////////
+
+var sum_sub_total;
+sum_sub_total = subtotal_veg + subtotal;
+document.getElementById("sum_sub_total").innerHTML = sum_sub_total;
 }
